dc8aad590fb9f541942df5525fd83b4369c890a3
bobbyv231 <bobbyv231@gmail.com>
2020-10-04 01:09:47 -0700

    commiting dist contents

---
+++ dist/precache-manifest.9aec8f539294daef675fa038e139564d.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fcbc3ebba4b2bdcf9d73",
+    "url": "css/app.3b49a340.css"
+  },
+  {
+    "revision": "0ecafd170ed61db5537f493aa224583c",
+    "url": "icon.jpg"
+  },
+  {
+    "revision": "5c97a8c011cfe2293967ecc14ccc6ea7",
+    "url": "img/Metaphysics-within-physics-cropped.5c97a8c0.jpg"
+  },
+  {
+    "revision": "a43d01c2e3dc994a18f4c8e872974db3",
+    "url": "img/New-foudations-for-physical-geometry-cropped.a43d01c2.jpg"
+  },
+  {
+    "revision": "7a2025f39effbf3e3bc4cb5e2eaf0c9f",
+    "url": "img/Philosophy-of-Physics-01-cropped.7a2025f3.jpg"
+  },
+  {
+    "revision": "738a2c6c4fb9c97dcd58b1526ab247c1",
+    "url": "img/Philosophy-of-Physics-01.738a2c6c.jpg"
+  },
+  {
+    "revision": "d0014bef672a3f97a88c9d1b276be1b8",
+    "url": "img/Philosophy-of-Physics-02-cropped.d0014bef.jpg"
+  },
+  {
+    "revision": "d1d491dab9ae6ddd11d9db5fb7897ffe",
+    "url": "img/geo-02.d1d491da.jpg"
+  },
+  {
+    "revision": "bc28bce05e1f7f65c37d081bb14b6ce3",
+    "url": "img/geo-03.bc28bce0.jpg"
+  },
+  {
+    "revision": "a1f56ea3c9515bf9c5c960f87b0e1964",
+    "url": "img/geo-04.a1f56ea3.jpg"
+  },
+  {
+    "revision": "eb7a99842b553ecbda4da351e0d25c5e",
+    "url": "img/logic.eb7a9984.jpg"
+  },
+  {
+    "revision": "e4ea37c8c3317d08cbfb6bbb6a120ce2",
+    "url": "img/physics.e4ea37c8.jpg"
+  },
+  {
+    "revision": "9ed37d433f3a7cfde67588e63aca4d7c",
+    "url": "img/profile.9ed37d43.jpg"
+  },
+  {
+    "revision": "e99a2d5da1e83734de95b3b39fa69ead",
+    "url": "img/quantum-cropped.e99a2d5d.jpg"
+  },
+  {
+    "revision": "878dc855af1676fbce3f2484d7fa7938",
+    "url": "img/thinker.878dc855.jpg"
+  },
+  {
+    "revision": "83510578f529b34b0d262690b2dd1750",
+    "url": "img/truth-and-paradox-cropped.83510578.jpg"
+  },
+  {
+    "revision": "bab3747264185e4302e6204331a4ff74",
+    "url": "index.html"
+  },
+  {
+    "revision": "fcbc3ebba4b2bdcf9d73",
+    "url": "js/app.b13210c2.js"
+  },
+  {
+    "revision": "6c8c40484318d8987266",
+    "url": "js/chunk-vendors.dc689403.js"
+  },
+  {
+    "revision": "5ad98c333e00e16f25436f95c151f787",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
